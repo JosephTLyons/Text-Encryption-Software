cb5201aeb128b083fa29076e1c9f93223da06f67
Joseph Lyons <joe_04_04@yahoo.com>
2016-07-07 02:11:46 -0400

    Minor code clean u-p

--- JTL Text Encryption/Double Encryption Read From Input.cpp
+++ JTL Text Encryption/Double Encryption Read From Input.cpp
@@ -11,8 +11,8 @@ char EncryptMessage(char KeyForEncrypting[], char Input);
 char DecryptMessage(char KeyForDecrypting[], char Input);
 
 const int ASCII_RANGE_SIZE_94 = 94;   //Range from ASCII value 32 to 126 = 94
-const int SHIFT_SET_32 = 32;          //Move range up 32 values form (0-93) to (32 to 126)
-const int PASSWORD_LENGHT_25 = 25;
+const int SHIFT_SET_32        = 32;   //Move range up 32 values form (0-93) to (32 to 126)
+const int PASSWORD_LENGHT_25  = 25;
 
 int main()
 {
@@ -33,25 +33,24 @@ int main()
         cin.ignore(1); //removes newline from input buffer
         
         EncryptDecryptMessage(Choice);
-        
     }
     
-    while (Choice==1 || Choice == 2);
+    while (Choice ==1 || Choice == 2);
 }
 
 void EncryptDecryptMessage(int Choice)
 {
     char Password[PASSWORD_LENGHT_25];
     
-    int  PasswordNumber=1;
+    int  PasswordNumber = 1;
     
-    char Key1[ASCII_RANGE_SIZE_94]={0};   //One larger 0 - 93, not using 0
-    char Key2[ASCII_RANGE_SIZE_94]={0};
-    char Key3[ASCII_RANGE_SIZE_94]={0};
+    char Key1[ASCII_RANGE_SIZE_94] = {0};//One larger 0 - 93, not using 0
+    char Key2[ASCII_RANGE_SIZE_94] = {0};
+    char Key3[ASCII_RANGE_SIZE_94] = {0};
     
     char InputCharacter1, InputCharacter2, InputCharacter3;
     
-    if (Choice==1)
+    if (Choice == 1)
         cout << "\nEnter an encryption password: ";
     
     else
@@ -76,7 +75,7 @@ void EncryptDecryptMessage(int Choice)
     
     FillKey(Key3);
     
-    if (Choice==1)
+    if (Choice == 1)
     {
         cout << "\nInput message\n";
         cout << "=============\n\n";
@@ -90,7 +89,7 @@ void EncryptDecryptMessage(int Choice)
     
     cin.get(InputCharacter1);
     
-    if (Choice==1)
+    if (Choice == 1)
     {
         cout << "\nEncrypted message\n";
         cout << "=================\n\n";
@@ -102,12 +101,13 @@ void EncryptDecryptMessage(int Choice)
         cout << "===============\n\n";
     }
     
-    if (Choice==1)
+    if (Choice == 1)
     {
         while (InputCharacter1!='\n')
         {
             InputCharacter2=EncryptMessage(Key1, InputCharacter1);
             InputCharacter3=EncryptMessage(Key2, InputCharacter2);
+            
             cout << EncryptMessage(Key3, InputCharacter3);
             cin.get(InputCharacter1);
         }
@@ -115,21 +115,22 @@ void EncryptDecryptMessage(int Choice)
     
     else
     {
-        while (InputCharacter1!='\n')
+        while (InputCharacter1 != '\n')
         {
             InputCharacter2=DecryptMessage(Key3, InputCharacter1);
             InputCharacter3=DecryptMessage(Key2, InputCharacter2);
+            
             cout << DecryptMessage (Key1, InputCharacter3);
             cin.get(InputCharacter1);
         }
     }
     
-    cout << endl << endl;
+    cout << "\n\n";
 }
 
 void WordToNumberConverter(int &PassNumber, char Password[])
 {
-    for (int i=0; Password[i]; ++i)
+    for (int i = 0; Password[i]; i++)
     {
         PassNumber += (int) Password[i];
         PassNumber += i;
@@ -138,9 +139,9 @@ void WordToNumberConverter(int &PassNumber, char Password[])
 
 void RunThroughRandomNumbers(int PasswordNum)//cycles through random numbers randomly
 {
-    int LoopLimit = rand()/PasswordNum;
+    int LoopLimit = rand() / PasswordNum;
     
-    for (int i=0; i<=LoopLimit; ++i)
+    for (int i = 0; i <= LoopLimit; ++i)
     {
         rand();
     }
@@ -151,15 +152,15 @@ void FillKey(char KeyBeingFilled[])
     int RandomNumber;
     int KeySpot, j;
     
-    for (KeySpot=0; KeySpot<ASCII_RANGE_SIZE_94; ++KeySpot)
+    for (KeySpot = 0; KeySpot < ASCII_RANGE_SIZE_94; ++KeySpot)
     {
-        RandomNumber = SHIFT_SET_32+rand()%ASCII_RANGE_SIZE_94;//0-93, then shifts up to 32-126 ASCII range
+        RandomNumber = SHIFT_SET_32 + rand() % ASCII_RANGE_SIZE_94;//0-93, then shifts up to 32-126 ASCII range
         
         for (j = 0; j <= KeySpot; ++j)//this bit of code checks to make sure new rand number isn't already used before
         {
             if (KeyBeingFilled[j] == RandomNumber)
             {
-                RandomNumber = SHIFT_SET_32+rand()%ASCII_RANGE_SIZE_94;//0-93, then shifts up to 32-126
+                RandomNumber = SHIFT_SET_32 + rand() % ASCII_RANGE_SIZE_94;//0-93, then shifts up to 32-126
                 j = -1;
             }
         }
@@ -170,12 +171,12 @@ void FillKey(char KeyBeingFilled[])
 
 char EncryptMessage(char KeyForEncrypting[], char Input)
 {
-    return KeyForEncrypting[((int) Input)-SHIFT_SET_32];//-32 to get range back into 1-95 (range of Key)
+    return KeyForEncrypting[((int) Input) - SHIFT_SET_32];//-32 to get range back into 1-95 (range of Key)
 }
 
 char DecryptMessage(char KeyForDecrypting[], char Input)
 {
-    for (int Count=0; Count<ASCII_RANGE_SIZE_94; ++Count)
+    for (int Count = 0; Count < ASCII_RANGE_SIZE_94; ++Count)
     {
         if ((int) Input == KeyForDecrypting[Count])
         {
